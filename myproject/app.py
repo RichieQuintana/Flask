87e955bc25d0d89f8dbb4d25b6ad02e434d68d18
RichieQuintana <103153905+RichieQuintana@users.noreply.github.com>
2024-09-30 16:05:14 -0500

    añadiendo routers

--- myproject/app.py
+++ myproject/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+from config import Config
+from extensions import db
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     register_resources(app)
+    register_extensions(app)
     return app
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app,db)
 
 def register_resources(app):
     @app.route('/home')
@@ -23,7 +32,7 @@ def register_resources(app):
 
 
 def register_resources(app):
-    @app.route('/')
+    @app.route('/', methods = ['GET'])
     def home():
         print("Rendering index.html")  
         return render_template('index.html')
